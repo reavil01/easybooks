4ef5a463391749bb37d9607038b6275760f4566f
reavil <reavil01@gmail.com>
2021-04-28 11:07:16 +0900

    Remove legacy code

--- src/main/kotlin/com/easybooks/demo/MemDB.kt
+++
@@ -1,8 +0,0 @@
-package com.easybooks.demo
-
-import com.easybooks.demo.domain.Company
-
-object MemDB {
-    val ledgerTable = HashMap<Long, Ledger>()
-    val clientTable = HashMap<String, Company>()
-}
